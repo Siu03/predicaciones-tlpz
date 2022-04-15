7f49c42967c9a2e73482cc789ddbf207177a4150
Siu03 <luisangelsiu2003@gmail.com>
2022-04-15 14:13:14 -0600

    error

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import 'semantic-ui-css/semantic.min.css'; //error
+import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
