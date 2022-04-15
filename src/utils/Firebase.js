5bb317070a712bd6e1ba038f92ea01b704f05a70
Siu03 <luisangelsiu2003@gmail.com>
2022-04-14 23:37:29 -0600

    Verificación de funcionamiento

---
+++ src/utils/Firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase/app";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAycVsoLNqjvP5EMo_4YPA0sw6NNCl3kSE",
+    authDomain: "predicaciones-tlpz.firebaseapp.com",
+    projectId: "predicaciones-tlpz",
+    storageBucket: "predicaciones-tlpz.appspot.com",
+    messagingSenderId: "427573088168",
+    appId: "1:427573088168:web:1fd4a95a1dc3e077f6179d",
+    measurementId: "G-9DFRLTCWDQ"
+};
+
+export default firebase.initializeApp(firebaseConfig);
