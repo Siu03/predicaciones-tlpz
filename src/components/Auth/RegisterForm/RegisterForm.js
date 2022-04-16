ebdf607664ec8ab911d93e89dbe32576c8cb1215
Siu03 <luisangelsiu2003@gmail.com>
2022-04-15 22:19:05 -0600

    main

--- src/components/Auth/RegisterForm/RegisterForm.js
+++ src/components/Auth/RegisterForm/RegisterForm.js
@@ -1,9 +1,76 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Button, Icon, Form, Input } from "semantic-ui-react";
+import firebase from "../../../utils/Firebase";
+import "firebase/auth";
 
-export default function RegisterForm() {
+//Importación de estilos
+
+import "./RegisterForm.scss";
+
+export default function RegisterForm(props) {
+const { setSelectedForm } = props;
+const [formData, setFormData] = useState(defaultValueForm());
+
+const onChange = e => {
+  console.log(e.target);
+}
+
+  const onSubmit = () => {
+    console.log("Formulario enviado.");
+    console.log(formData);
+  }
+
   return (
-    <div>
-        <h1>RegisterForm...</h1>
+    <div className='register-form'>
+        <h1>Empieza a escuchar las predicaciones locales.</h1>
+        <Form onSubmit={onSubmit} onChange={onChange}>
+          <Form.Field>
+            <Input 
+              type="text"
+              name="email"
+              placeholder="Correo electrónico"
+              icon="mail outline"
+              
+              // error= {}
+            />
+          </Form.Field>
+          <Form.Field>
+            <Input 
+              type="password"
+              name="password"
+              placeholder="Contraseña"
+              icon="eye"
+              
+              // error= {}
+            />
+          </Form.Field>
+          <Form.Field>
+            <Input 
+              type="text"
+              name="username"
+              placeholder="¿Cómo deberíamos llamarte?"
+              icon="user circle outline"
+              
+              // error= {}
+            />
+          </Form.Field>
+          <Button type="submit">Continuar</Button>
+        </Form>
+
+        <div className='register-form__options'>
+            <p onClick={ () => setSelectedForm(null)}>Volver</p>
+            <p>
+              ¿Ya tienes una cuenta? <span onClick={() => setSelectedForm("login")}>Iniciar sesión</span>
+            </p>
+        </div>
     </div>
-  )
+  );
+}
+
+function defaultValueForm() {
+  return {
+    email: "",
+    password:"",
+    username:""
+  }
 }
