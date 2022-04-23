39fa7383b0c68cbec414715061100565d197f88e
Siu03 <luisangelsiu2003@gmail.com>
2022-04-23 17:33:38 -0600

    MenuLeft - Añadi permisos y apartado de menu.

--- src/components/MenuLeft/MenuLeft.js
+++ src/components/MenuLeft/MenuLeft.js
@@ -1,29 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Menu, Icon } from "semantic-ui-react";
 import { Link, withRouter } from "react-router-dom";
+import { isUserAdmin } from "../../utils/Api";
+
 import "./MenuLeft.scss";
 
-export default function MenuLeft(props) {
+function MenuLeft(props) {
   const { user } = props;
+  //const [activeMenu, setActiveMenu] = useState(location.pathname);
+  const [activeMenu, setActiveMenu] = useState(window.location.pathname);
+  const [userAdmin, setUserAdmin] = useState(false);  
+
+  useEffect (() => {
+    isUserAdmin(user.uid).then(response => {
+      setUserAdmin(response);
+    });
+  }, [user]);
+
+  const handlerMenu = (e, menu) => {
+    setActiveMenu(menu.to);
+  };
 
   return (
     <Menu className="menu-left" vertical>
       <div className="top">
-        <Menu.Item>
-          <Icon name="home">Inicio</Icon>
+        <Menu.Item as={Link} to="/" active={activeMenu === "/"} onClick={handlerMenu}>
+          <Icon name="home" />Inicio
         </Menu.Item>
-        <Menu.Item>
-          <Icon name="music">Hermanos</Icon>
+        <Menu.Item as={Link} to="/hermanos" active={activeMenu === "/hermanos"} onClick={handlerMenu}>
+          <Icon name="music" />Hermanos
         </Menu.Item>
       </div>
+      {userAdmin && (
       <div className="footer">
         <Menu.Item>
-            <Icon name="plus square outline">Añadir predicador</Icon>
+            <Icon name="plus square outline" />Añadir predicador
         </Menu.Item>
         <Menu.Item>
-            <Icon name="plus square outline">Añadir predicación</Icon>
+            <Icon name="plus square outline" />Añadir predicación
         </Menu.Item>
       </div>
+      )}
     </Menu>
-  )
+  );
 }
+export default withRouter(MenuLeft);
